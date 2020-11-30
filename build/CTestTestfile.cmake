fca3caca24ea30f05df989f06a62e1146214d7bd
Kushagra <wadhwakushagra01@gmail.com>
2020-12-01 02:06:42 +0530

    Init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/kush/Desktop/sem3/en203/src
+# Build directory: /home/kush/Desktop/sem3/en203/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("simulation")
